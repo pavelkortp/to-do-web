e59dd464a9a539f07a94fbd643461447d46969fd
pavelkortp <pavelkortp@gmail.com>
2023-11-02 18:09:23 +0200

    adds put request, delete not work

--- dist/ItemsRouter.js
+++ dist/ItemsRouter.js
@@ -8,16 +8,28 @@ router
 })
     .post((req, res) => {
     const task = req.body;
-    task.checked = 'false';
     task.id = data.items
         .reduce((sum, cur) => {
         return sum + cur.id;
     }, 0);
+    task.checked = 'false';
     data.items.push(task);
     res.json({ "id": task.id });
 })
     .put((req, res) => {
+    const body = req.body;
+    const task = data.items.find((e) => e.id == body.id);
+    if (task == undefined) {
+        res.status(500).send('task not found');
+        return;
+    }
+    task.checked = body.checked;
+    task.text = body.text;
+    res.json({ "ok": "true" });
 })
     .delete((req, res) => {
+    const body = req.body;
+    data.items = data.items.filter((e) => e.id != body.id);
+    res.json({ "ok": "true" });
 });
 export { router };
--- src/ItemsRouter.ts
+++ src/ItemsRouter.ts
@@ -11,18 +11,29 @@ router
     .post((req: Request, res: Response) => {
         const task: { id: number, text: string, checked: string } = req.body;
 
-        task.checked = 'false';
         task.id = data.items
             .reduce((sum: number, cur: { id: number }) => {
                 return sum + cur.id;
             }, 0);
+        task.checked = 'false';
+
         data.items.push(task);
         res.json({"id": task.id});
     })
     .put((req: Request, res: Response) => {
-
+        const body: { id: number, text: string, checked: string } = req.body;
+        const task = data.items.find((e: { id: number }) => e.id == body.id);
+        if (task == undefined) {
+            res.status(500).send('task not found');
+            return;
+        }
+        task.checked = body.checked;
+        task.text = body.text;
+        res.json({"ok": "true"});
     })
     .delete((req: Request, res: Response) => {
-
+        const body: { id: number } = req.body;
+        data.items = data.items.filter((e: { id: number }) => e.id != body.id);
+        res.json({"ok": "true"});
     });
-export{router};
+export {router};
