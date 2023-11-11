ac23f49fbc4f35d67e9ac098c6696b62b182c145
Pavlo <pavelkortp@gmail.com>
2023-11-11 15:22:33 +0200

    adds comments to config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,24 @@
+/**
+ * DB user's login.
+ */
 export const username: string = '';
+
+/**
+ * DB user's password.
+ */
 export const pass: string = '';
+
+/**
+ * Database name.
+ */
 export const db_name: string = '';
-export const collection: string = '';
+
+/**
+ * Name of collection in DB
+ */
+export const collection_name: string = '';
+
+/**
+ * Uri to MongoDB
+ */
 export const uri: string = ``;
