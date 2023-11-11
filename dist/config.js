dd198b78f31db570c85d615404661b5540fa7c0e
Pavlo <pavelkortp@gmail.com>
2023-11-11 15:19:16 +0200

    removes build folder and edits .gitignore

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-export const username = 'pavlo';
-export const pass = '0kzBb4qr6s55uBou';
-export const db_name = 'TODO';
-export const collection_name = 'to-do-list-users';
-export const uri = `mongodb+srv://${username}:${pass}@todo.kcnj1lt.mongodb.net/?retryWrites=true&w=majority`;
